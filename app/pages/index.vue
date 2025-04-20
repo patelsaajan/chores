7826ea906be01b07d8bad48f3c1e1e2001ce8142
Saajan Patel <saajan.patel@hoppstudio.com>
2025-04-20 13:36:52 +0100

    initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container mx-auto">
+        <div class="text-primary text-5xl font-bold">
+            hello world
+        </div>
+
+        <UButton color="primary" size="xl">
+            Click me
+        </UButton>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
